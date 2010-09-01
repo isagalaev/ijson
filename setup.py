53881c2032416f7fef893af514a070af3f8098e5
Ivan Sagalaev <isagalaev@yandex-team.ru>
2010-09-01 16:37:36 +0400

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'ijson',
-    version = '0.1.0',
+    version = '0.2.0',
     author = 'Ivan Sagalaev',
     author_email = 'Maniac@SoftwareManiacs.Org',
     packages = ['ijson'],
