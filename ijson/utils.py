7ce209b0caceb703c268a4726a3452ccc551ace2
Ivan Sagalaev <isagalaev@yandex-team.ru>
2010-09-02 18:16:56 +0400

    ObjectBuilder. I just wrote this code and it scares the hell out of me...

---
+++ ijson/utils.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+class ObjectBuilder(object):
+    def __init__(self):
+        def initial_set(value):
+            self.value = value
+        self.containers = [initial_set]
+
+    def event(self, event, value):
+        if event == 'map_key':
+            self.key = value
+        elif event == 'start_map':
+            map = {}
+            self.containers[-1](map)
+            def setter(value):
+                map[self.key] = value
+            self.containers.append(setter)
+        elif event == 'start_array':
+            array = []
+            self.containers[-1](array)
+            self.containers.append(array.append)
+        elif event == 'end_array' or event == 'end_map':
+            self.containers.pop()
+        else:
+            self.containers[-1](value)
