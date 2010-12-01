d4989be2a12d6f047b9f9df38b8397411ab079e7
andrewimam@hotmail.com <>
2010-12-01 03:35:15 -0800

    ctypes.cdll assume int return types and causes segfault on 64-bit (ptr truncation?)

--- ijson/lib.py
+++ ijson/lib.py
@@ -1,6 +1,10 @@
-from ctypes import cdll, util
+from ctypes import cdll, util, c_char, POINTER
 
 name = util.find_library('yajl')
 if name is None:
     raise Exception('YAJL shared object not found.')
 yajl = cdll.LoadLibrary(name)
+
+yajl.yajl_alloc.restype = POINTER(c_char)
+yajl.yajl_gen_alloc.restype = POINTER(c_char)
+yajl.yajl_gen_alloc2.restype = POINTER(c_char)
