ad4e1d635fd4dd8cfcb2ac9f15eb3551c0e25ff9
Ivan Sagalaev <Maniac@SoftwareManiacs.Org>
2010-12-03 16:31:32 +0300

    Workaround for find_library on Ubuntu Hardy 64

--- ijson/lib.py
+++ ijson/lib.py
@@ -1,6 +1,14 @@
 from ctypes import cdll, util, c_char, POINTER
 
 name = util.find_library('yajl')
+
+# Temporary hack for Hardy 64. find_library doesn't find this file for some
+# reason.
+import os
+hardy64_name = '/usr/lib/libyajl.so.1'
+if os.path.exists(hardy64_name):
+    name = hardy64_name
+
 if name is None:
     raise Exception('YAJL shared object not found.')
 yajl = cdll.LoadLibrary(name)
