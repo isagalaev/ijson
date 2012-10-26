4283b1f96381fc6b187a94851afc769a06b75d4d
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-10-26 15:18:12 -0700

    Added some classifiers to setup.py and reverted `license` field back to the filename.

--- setup.py
+++ setup.py
@@ -8,7 +8,14 @@ setup(
     author_email = 'maniac@softwaremaniacs.org',
     packages = ['ijson', 'ijson.backends'],
     url = 'https://github.com/isagalaev/ijson',
-    license = open('LICENSE.txt').read(),
+    license = 'LICENSE.txt',
     description = 'Iterative JSON parser with a standard Python iterator interface',
     long_description = open('README.rst').read(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
