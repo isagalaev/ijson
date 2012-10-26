e399ce77cd3a54ba0b8f7490bf86e0641112efbe
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-10-25 22:47:03 -0700

    Version 1.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name = 'ijson',
-    version = '0.8.0',
+    version = '1.0',
     author = 'Ivan Sagalaev',
-    author_email = 'Maniac@SoftwareManiacs.Org',
+    author_email = 'maniac@softwaremaniacs.org',
     packages = ['ijson', 'ijson.backends'],
     url = 'https://github.com/isagalaev/ijson',
-    license = 'LICENSE.txt',
+    license = open('LICENSE.txt').read(),
     description = 'Iterative JSON parser with a standard Python iterator interface',
     long_description = open('README.rst').read(),
 )
