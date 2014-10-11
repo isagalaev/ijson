9cff9a90357aa4344360958227064df77912b42f
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-10-11 00:18:48 -0700

    Switching setup.py to setuptools.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-# -*- coding:utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'ijson',
     version = '1.1',
     author = 'Ivan Sagalaev',
     author_email = 'maniac@softwaremaniacs.org',
-    packages = ['ijson', 'ijson.backends'],
     url = 'https://github.com/isagalaev/ijson',
-    license = 'LICENSE.txt',
+    license = 'BSD',
     description = 'Iterative JSON parser with a standard Python iterator interface',
     long_description = open('README.rst').read(),
+
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
+
+    packages = find_packages(),
 )
