b0873d71b82e2af8983511b0ea3306dbe48c145c
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-02-16 16:59:35 -0800

    Version 2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ijson',
-    version = '2.0',
+    version = '2.1',
     author = 'Ivan Sagalaev',
     author_email = 'maniac@softwaremaniacs.org',
     url = 'https://github.com/isagalaev/ijson',
